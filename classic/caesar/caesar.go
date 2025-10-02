bc52397853305a9793ed549c0edd617def56cd62
Borhan <borhan.javadian@proton.me>
2025-10-02 23:52:36 +0300

    Basic structure, will code later

---
+++ classic/caesar/caesar.go
@@ -0,0 +1,6 @@
+package caesar
+
+// Encrypt shifts the input text by 'key' positions (Caesar cipher).
+func Encrypt(text string, key int) string {
+	return ""
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/borhanxj/encsuite-go
+
+go 1.23.0
